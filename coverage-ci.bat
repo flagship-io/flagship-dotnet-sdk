166e9674cfffcce3620c3259f9a495b10cea51ef
guigithub <guillaume.jacquart@abtasty.com>
2021-05-11 10:54:19 +0200

    change register profile for open cover

--- coverage-ci.bat
+++ coverage-ci.bat
@@ -2,4 +2,4 @@ set current_dir=%~dp0
 set VSTest="%current_dir%packages\Microsoft.TestPlatform.16.8.3\tools\net451\Common7\IDE\Extensions\TestPlatform\VSTest.Console.exe"
 set NUGETFolder=%current_dir%packages
  
-"%NUGETFolder%\OpenCover.4.7.922\tools\OpenCover.Console.exe" -target:%VSTest% -targetargs:"%current_dir%Flagship.Tests\bin\Debug\Flagship.Tests.dll" -output:"%current_dir%CoverageResults.xml" -register:user
+"%NUGETFolder%\OpenCover.4.7.922\tools\OpenCover.Console.exe" -target:%VSTest% -targetargs:"%current_dir%Flagship.Tests\bin\Debug\Flagship.Tests.dll" -output:"%current_dir%CoverageResults.xml" -register:Path64

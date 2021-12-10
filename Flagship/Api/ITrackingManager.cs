4f19b5d6e42426a088e55f7b85b6d6a3cb0d9dfc
Merveille KITOKO <merkitoko@gmail.com>
2021-12-10 08:32:48 +0100

    add ITracking interface

---
+++ Flagship/Api/ITrackingManager.cs
@@ -0,0 +1,22 @@
+﻿using Flagship.Config;
+using Flagship.FsVisitor;
+using Flagship.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flagship.Api
+{
+    public interface ITrackingManager
+    {
+        public FlagshipConfig Config { get; set; }
+
+        public Task SendActive(VisitorDelegateAbstract visitor, FlagDTO flag);
+
+        public Task SendHit(object hit);
+
+        public Task SendConsentHit(VisitorDelegateAbstract visitor);
+    }
+}

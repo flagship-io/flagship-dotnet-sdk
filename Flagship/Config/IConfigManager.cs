0e81a8e3d0e3d3daab9e22e73a0580c9250822db
Merveille KITOKO <merkitoko@gmail.com>
2021-12-10 08:33:14 +0100

    add IConfigManager interface

--- Flagship/Config/IConfigManager.cs
+++ Flagship/Config/IConfigManager.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Flagship.Api;
+using Flagship.Decision;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,11 @@ namespace Flagship.Config
 {
     public interface IConfigManager
     {
+        public IDecisionManager DecisionManager { get; set; }
+
+        public FlagshipConfig Config { get; set; }
+
+        public ITrackingManager TrackingManager { get; set; }
+
     }
 }

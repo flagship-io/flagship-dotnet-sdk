1aa62c50019f8acb12a0e0a1e8830a5ab1998902
Merveille KITOKO <merkitoko@gmail.com>
2021-12-14 13:42:21 +0100

    implement SendActive method in TrackingManager

--- Flagship/Api/TrackingManager.cs
+++ Flagship/Api/TrackingManager.cs
@@ -1,10 +1,13 @@
 ﻿using Flagship.Config;
+using Flagship.Enums;
 using Flagship.FsVisitor;
 using Flagship.Model;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -23,7 +26,50 @@ namespace Flagship.Api
 
         public Task SendActive(VisitorDelegateAbstract visitor, FlagDTO flag)
         {
-            throw new NotImplementedException();
+            return Task.Factory.StartNew(async () =>
+            {
+                try
+                {
+                    var url = $"{Constants.BASE_API_URL}activate";
+                    var requestMessage = new HttpRequestMessage(HttpMethod.Post, url);
+
+                    requestMessage.Headers.Add(Constants.HEADER_X_API_KEY, Config.ApiKey);
+                    requestMessage.Headers.Add(Constants.HEADER_X_SDK_CLIENT, Constants.SDK_LANGUAGE);
+                    requestMessage.Headers.Add(Constants.HEADER_X_SDK_VERSION, Constants.SDK_VERSION);
+                    requestMessage.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue(Constants.HEADER_APPLICATION_JSON));
+
+                    var postData = new Dictionary<string, object>
+                    {
+                        [Constants.VISITOR_ID_API_ITEM] = visitor.VisitorId,
+                        [Constants.VARIATION_ID_API_ITEM] = flag.VariationId,
+                        [Constants.VARIATION_GROUP_ID_API_ITEM] = flag.VariationGroupId,
+                        [Constants.CUSTOMER_ENV_ID_API_ITEM] = Config.EnvId
+                    };
+
+                    if (!string.IsNullOrWhiteSpace(visitor.VisitorId) && !string.IsNullOrWhiteSpace(visitor.AnonymousId))
+                    {
+                        postData[Constants.VISITOR_ID_API_ITEM] = visitor.VisitorId;
+                        postData[Constants.ANONYMOUS_ID] = visitor.AnonymousId;
+                    }
+                    else
+                    {
+                        postData[Constants.VISITOR_ID_API_ITEM] = visitor.AnonymousId ?? visitor.VisitorId;
+                        postData[Constants.ANONYMOUS_ID] = null;
+                    }
+
+                    var postDatajson = JsonConvert.SerializeObject(postData);
+
+                    var stringContent = new StringContent(postDatajson, Encoding.UTF8, Constants.HEADER_APPLICATION_JSON);
+
+                    requestMessage.Content = stringContent;
+
+                    var response = await HttpClient.SendAsync(requestMessage);
+                }
+                catch (Exception ex)
+                {
+                    Utils.Log.LogError(Config, ex.Message, "SendActive");
+                }
+            });
         }
 
         public Task SendConsentHit(VisitorDelegateAbstract visitor)

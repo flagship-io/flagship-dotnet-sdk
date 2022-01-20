4e314420ea0a12296695da77f5e2dd262aad1624
Merveille KITOKO <merkitoko@gmail.com>
2022-01-20 17:36:27 +0100

    add BucketingDTO

---
+++ Flagship/Model/Bucketing/BucketingDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flagship.Model.Bucketing
+{
+    internal class BucketingDTO
+    {
+        public bool? Panic { get; set; }
+        public IEnumerable<Campaign> Campaigns { get; set; }
+    }
+}

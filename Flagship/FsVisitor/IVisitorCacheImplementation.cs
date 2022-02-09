8102f8b59ff4edf358192ac29494ca6181897f29
Merveille KITOKO <merkitoko@gmail.com>
2022-02-09 17:14:47 +0100

    add IVisitorCacheImplementation interface

---
+++ Flagship/FsVisitor/IVisitorCacheImplementation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flagship.FsVisitor
+{
+    public interface IVisitorCacheImplementation
+    {
+        /// <summary>
+        /// This method is called when the SDK needs to cache visitor information in your database.
+        /// </summary>
+        /// <param name="visitorId">Visitor ID</param>
+        /// <param name="data">Visitor data JSON</param>
+        Task CacheVisitor(string visitorId, string data);
+
+        /// <summary>
+        /// This method is called when the SDK needs to get the visitor information corresponding to visitor ID from your database.
+        /// </summary>
+        /// <param name="visitorId">Visitor ID</param>
+        /// <returns></returns>
+        Task<string> LookupVisitor(string visitorId);
+
+        /// <summary>
+        /// This method is called when the SDK needs to erase the visitor information corresponding to visitor ID in your database.
+        /// </summary>
+        /// <param name="visitorId">Visitor ID</param>
+        Task FlushVisitor(string visitorId);
+    }
+}

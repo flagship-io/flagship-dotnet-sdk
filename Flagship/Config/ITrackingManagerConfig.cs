103cd490789a3f3dfca48ad41169807b4002761d
Heihokon <neoshark22@gmail.com>
2022-09-13 12:49:10 +0100

    add ITrackingManagerConfig interface

---
+++ Flagship/Config/ITrackingManagerConfig.cs
@@ -0,0 +1,24 @@
+﻿using Flagship.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flagship.Config
+{
+    public interface ITrackingManagerConfig
+    {
+        /// <summary>
+        /// Define the time intervals the SDK will use to send tracking batches.
+        /// </summary>
+        public int BatchIntervals { get; set; }
+
+        /// <summary>
+        /// Define the maximum number of tracking hit that each batch can contain.
+        /// </summary>
+        public int BatchLength { get; set; }
+        public BatchStrategy BatchStrategy { get; set; }
+
+    }
+}

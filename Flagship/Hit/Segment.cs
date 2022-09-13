9ad0f27cfe6684d7439c6cf2c933cd8faf2c3dff
Heihokon <neoshark22@gmail.com>
2022-09-13 12:48:15 +0100

    add Segment hit

---
+++ Flagship/Hit/Segment.cs
@@ -0,0 +1,40 @@
+﻿using Flagship.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flagship.Hit
+{
+    internal class Segment : HitAbstract
+    {
+        public const string ERROR_MESSAGE = "data property is required";
+        public Dictionary<string, object> Data { get; set; }
+
+        public Segment(Dictionary<string, object> data):base(HitType.CONTEXT)
+        {
+            Data = data;
+        }
+
+        internal override bool IsReady(bool checkParent = true)
+        {
+            return base.IsReady(checkParent) && Data!=null;
+        }
+
+        internal override IDictionary<string, object> ToApiKeys()
+        {
+            return new Dictionary<string, object>()
+            {
+                ["visitorId"] = VisitorId,
+                ["data"] = Data,
+                ["type"] = $"{Type}",
+            };
+        }
+
+        internal override string GetErrorMessage()
+        {
+            return ERROR_MESSAGE;
+        }
+    }
+}

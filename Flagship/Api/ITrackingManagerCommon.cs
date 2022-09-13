9e841a434ac42b7c66d731af0d33ac4ab7d75291
Heihokon <neoshark22@gmail.com>
2022-09-13 12:50:53 +0100

    add ITrackingManagerCommon interface

---
+++ Flagship/Api/ITrackingManagerCommon.cs
@@ -0,0 +1,16 @@
+﻿using Flagship.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flagship.Api
+{
+    internal interface ITrackingManagerCommon
+    {
+        public FlagshipConfig Config { get; set; }
+
+        public void Add(Hit.HitAbstract hit);
+    }
+}

a68840641db3e8b1c02cdec3873c150ca61f4064
Heihokon <neoshark22@gmail.com>
2022-09-13 12:50:01 +0100

    add BatchStrategy enum

---
+++ Flagship/Enums/BatchStrategy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flagship.Enums
+{
+    public enum BatchStrategy
+    {
+        CONTINUOUS_CACHING,
+        PERIODIC_CACHING,
+        NO_BATCHING
+    }
+
+}

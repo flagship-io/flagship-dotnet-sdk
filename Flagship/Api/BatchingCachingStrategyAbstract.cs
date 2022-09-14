4cda05cc400ba400b73d0f649ff0193dace54ed1
Heihokon <neoshark22@gmail.com>
2022-09-14 13:54:49 +0100

    implement BatchingContinuousCachingStrategy

--- Flagship/Api/BatchingCachingStrategyAbstract.cs
+++ Flagship/Api/BatchingCachingStrategyAbstract.cs
@@ -18,9 +18,14 @@ namespace Flagship.Api
         public const string HIT_DATA_CACHED = "Hit data has been saved into database : {0}";
         public const string PROCESS_FLUSH_HIT = "FLUSH HIT";
         public const string HIT_DATA_FLUSHED = "The following hit keys have been flushed from database : {0}";
-        public const string FS_CONSENT = "fs_consent";
         public const string ADD_HIT = "ADD HIT";
         public const string HIT_ADDED_IN_QUEUE = "The hit has been added to the pool queue : {0}";
+        public const string BATCH_SENT_SUCCESS = "Batch hit has been sent : {0}";
+        public const string SEND_BATCH = "SEND BATCH";
+        public const string SEND_ACTIVATE = "SEND ACTIVATE";
+        public const string SEND_SEGMENT_HIT = "SEND SEGMENT HIT";
+        public const string HIT_SENT_SUCCESS = "hit has been sent : {0}";
+
         public FlagshipConfig Config { get ; set ; }
         public HttpClient HttpClient { get; set; }
 
@@ -38,6 +43,8 @@ namespace Flagship.Api
         abstract public Task SendBatch();  
 
         abstract public Task NotConsent(string visitorId);
+
+        abstract public Task SendActivateAndSegmentHit(IEnumerable<HitAbstract> hits);
 
         public async Task CacheHitAsync(Dictionary<string, HitAbstract> hits)
         {
--- Flagship/Api/BatchingContinuousCachingStrategy.cs
+++ Flagship/Api/BatchingContinuousCachingStrategy.cs
@@ -1,10 +1,12 @@
 ﻿using Flagship.Config;
+using Flagship.Enums;
 using Flagship.Hit;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,8 +24,7 @@ namespace Flagship.Api
             var hitKey = $"{hit.VisitorId}:{Guid.NewGuid()}";
             hit.Key = hitKey;
             await AddHitWithKey(hitKey, hit);
-            var sdkName = Assembly.GetExecutingAssembly().FullName;
-            if (hit is Event eventHit && eventHit.Action == FS_CONSENT && eventHit.Label == $"{sdkName}:false")
+            if (hit is Event eventHit && eventHit.Action == Constants.FS_CONSENT && eventHit.Label == $"{Constants.SDK_LANGUAGE}:false")
             {
                 await NotConsent(hit.VisitorId);
             }
@@ -32,23 +33,162 @@ namespace Flagship.Api
 
         protected async Task AddHitWithKey(string key, HitAbstract hit)
         {
-            HitsPoolQueue[key]= hit;
-            await CacheHitAsync(HitsPoolQueue);
+            HitsPoolQueue[key] = hit;
+            await CacheHitAsync(new Dictionary<string, HitAbstract>() { { key, hit } });
         }
 
-        public override Task NotConsent(string visitorId)
+        public override async Task NotConsent(string visitorId)
         {
-            var keys = HitsPoolQueue.Where(x=> !(x.Value is Event eventHit && eventHit.Action == FS_CONSENT) && x.Key.Contains(visitorId));
-            var keysToFlush = new List<string>();
+            var keys = HitsPoolQueue.Where(x => !(x.Value is Event eventHit && eventHit.Action == Constants.FS_CONSENT) && x.Key.Contains(visitorId)).Select(x => x.Key);
+
             foreach (var item in keys)
             {
+                HitsPoolQueue.Remove(item);
+            }
+            if (keys.Any())
+            {
+                return;
+            }
+            await FlushHitsAsync(keys.ToArray());
+        }
 
+        public override async Task SendActivateAndSegmentHit(IEnumerable<HitAbstract> hits)
+        {
+            var requestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+            };
+
+            var sdkVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+            requestMessage.Headers.Add(Constants.HEADER_X_API_KEY, Config.ApiKey);
+            requestMessage.Headers.Add(Constants.HEADER_X_SDK_CLIENT, Constants.SDK_LANGUAGE);
+            requestMessage.Headers.Add(Constants.HEADER_X_SDK_VERSION, sdkVersion);
+            requestMessage.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue(Constants.HEADER_APPLICATION_JSON));
+
+            var hitKeys = new List<string>();
+
+            foreach (var hit in hits)
+            {
+                HitsPoolQueue.Remove(hit.Key);
+                var requestBody = hit.ToApiKeys();
+                var isActivateHit = hit.Type == HitType.ACTIVATE;
+                var url = Constants.BASE_API_URL;
+                url += isActivateHit ? "activate" : $"{Config.EnvId}/events";
+                requestMessage.RequestUri = new Uri(url, UriKind.RelativeOrAbsolute);
+                var tag = isActivateHit ? SEND_ACTIVATE : SEND_SEGMENT_HIT;
+
+                try
+                {
+                    var postDatajson = JsonConvert.SerializeObject(requestBody);
+
+                    var stringContent = new StringContent(postDatajson, Encoding.UTF8, Constants.HEADER_APPLICATION_JSON);
+
+                    requestMessage.Content = stringContent;
+
+                    await HttpClient.SendAsync(requestMessage);
+
+                    hitKeys.Add(hit.Key);
+
+                    Logger.Log.LogInfo(Config, string.Format(HIT_SENT_SUCCESS, JsonConvert.SerializeObject(requestBody)), tag);
+                }
+                catch (Exception ex)
+                {
+                    await AddHitWithKey(hit.Key, hit);
+                    Logger.Log.LogError(Config, Utils.Utils.ErrorFormat(ex.Message, new
+                    {
+                        url,
+                        headers = new Dictionary<string, string>
+                        {
+                            {Constants.HEADER_X_API_KEY, Config.ApiKey},
+                            {Constants.HEADER_X_SDK_CLIENT, Constants.SDK_LANGUAGE },
+                            {Constants.HEADER_X_SDK_VERSION, sdkVersion }
+                        },
+                        body = requestBody
+                    }), tag);
+                }
+            }
+
+            if (hitKeys.Any())
+            {
+                await FlushHitsAsync(hitKeys.ToArray());
             }
         }
 
-        public override Task SendBatch()
+        public override async Task SendBatch()
         {
-            throw new NotImplementedException();
+            var batch = new Batch()
+            {
+                Config = Config
+            };
+
+            var count = 0;
+
+            var activateAndSegmentHits = new List<HitAbstract>();
+
+            var hitKeysToRemove = new List<string>();
+
+            foreach (var item in HitsPoolQueue)
+            {
+                if (item.Value.Type == HitType.ACTIVATE || item.Value.Type == HitType.CONTEXT)
+                {
+                    activateAndSegmentHits.Add(item.Value);
+                    continue;
+                }
+                count++;
+                var batchSize = JsonConvert.SerializeObject(batch).Length;
+                if (batchSize > Constants.BATCH_MAX_SIZE || count > Config.TrackingMangerConfig.BatchLength)
+                {
+                    break;
+                }
+                batch.Hits.Add(item.Value);
+                hitKeysToRemove.Add(item.Key);
+            }
+
+            await SendActivateAndSegmentHit(activateAndSegmentHits);
+
+            if (!batch.Hits.Any())
+            {
+                return;
+            }
+
+            foreach (var key in hitKeysToRemove)
+            {
+                HitsPoolQueue.Remove(key);
+            }
+
+            var requestBody = batch.ToApiKeys();
+
+            try
+            {
+                var requestMessage = new HttpRequestMessage(HttpMethod.Post, Constants.HIT_EVENT_URL);
+
+                requestMessage.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue(Constants.HEADER_APPLICATION_JSON));
+
+                var postDatajson = JsonConvert.SerializeObject(requestBody);
+
+                var stringContent = new StringContent(postDatajson, Encoding.UTF8, Constants.HEADER_APPLICATION_JSON);
+
+                requestMessage.Content = stringContent;
+
+                await HttpClient.SendAsync(requestMessage);
+
+                Logger.Log.LogInfo(Config, string.Format(BATCH_SENT_SUCCESS, JsonConvert.SerializeObject(requestBody)), SEND_BATCH);
+
+                await FlushHitsAsync(hitKeysToRemove.ToArray());
+            }
+            catch (Exception ex)
+            {
+                foreach (var item in batch.Hits)
+                {
+                    await AddHitWithKey(item.Key, item);
+                }
+                Logger.Log.LogError(Config, Utils.Utils.ErrorFormat(ex.Message, new
+                {
+                    url = Constants.HIT_EVENT_URL,
+                    body = requestBody
+                }), SEND_BATCH);
+            }
         }
     }
 }

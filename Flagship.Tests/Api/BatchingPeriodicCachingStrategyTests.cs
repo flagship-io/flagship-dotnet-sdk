fceb521e2fd4b11a0903fdf88ac81a7b0bc8a6c7
Heihokon <neoshark22@gmail.com>
2022-10-18 12:30:23 +0100

    add BatchingPeriodicCachingStrategyTests test class

---
+++ Flagship.Tests/Api/BatchingPeriodicCachingStrategyTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Flagship.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flagship.Api.Tests
+{
+    [TestClass()]
+    public class BatchingPeriodicCachingStrategyTests
+    {
+        [TestMethod()]
+        public void BatchingPeriodicCachingStrategyTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void NotConsentTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ActivateFlagTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SendBatchTest()
+        {
+            Assert.Fail();
+        }
+    }
+}

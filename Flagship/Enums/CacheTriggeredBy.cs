6a49425b4a08a047a7ba12b5cae231111b1459a3
Heihokon <neoshark22@gmail.com>
2022-11-11 08:58:31 +0100

    add CacheTriggeredBy enu,

---
+++ Flagship/Enums/CacheTriggeredBy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flagship.Enums
+{
+    internal enum CacheTriggeredBy
+    {
+        Timer,
+        BatchLength,
+        Flush,
+        ActivateLength,
+        DirectHit,
+        DirectActivate
+    }
+}

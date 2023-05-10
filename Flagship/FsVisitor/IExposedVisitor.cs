40d963e663a6412691b30e211ee4150531e21e6f
Heihokon <neoshark22@gmail.com>
2023-05-10 16:09:53 -0500

    add IExposedVisitor interface

---
+++ Flagship/FsVisitor/IExposedVisitor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flagship.FsVisitor
+{
+    public interface IExposedVisitor
+    {
+        string Id { get; }
+        string AnonymoudId { get; }
+        IDictionary<string, object> Context { get; }
+    }
+}

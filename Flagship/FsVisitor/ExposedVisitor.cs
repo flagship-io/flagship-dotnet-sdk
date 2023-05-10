ea27277bf9dd12a0179cd29debbc5e5a6ed29575
Heihokon <neoshark22@gmail.com>
2023-05-10 16:10:15 -0500

    add ExposedVisitor class

---
+++ Flagship/FsVisitor/ExposedVisitor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flagship.FsVisitor
+{
+    internal class ExposedVisitor : IExposedVisitor
+    {
+        public string Id { get ;  set; }
+        public string AnonymoudId { get; set; }
+        public IDictionary<string, object> Context { get ; set ; }
+
+        public ExposedVisitor(string id, string anonymoudId, IDictionary<string, object> context)
+        {
+            Id = id;
+            AnonymoudId = anonymoudId;
+            Context = context;
+        }
+    }
+}

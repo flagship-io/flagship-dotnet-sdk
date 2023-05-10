e8da77e86d1e89ff3cb2630468fd7aac5e77f13b
Heihokon <neoshark22@gmail.com>
2023-05-10 16:08:05 -0500

    add IExposedFlag interface

---
+++ Flagship/FsFlag/IExposedFlag.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flagship.FsFlag
+{
+    public interface IExposedFlag
+    {
+        string Key { get; }
+        object Value { get; }
+        object DefaultValue { get; }
+        IFlagMetadata Metadata { get; }
+
+    }
+}

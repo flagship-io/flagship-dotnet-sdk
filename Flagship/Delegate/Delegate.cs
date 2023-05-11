0d5a8452dd9b131264b71e75a1b471bab6345836
Heihokon <neoshark22@gmail.com>
2023-05-11 15:34:09 -0500

    add docs

--- Flagship/Delegate/Delegate.cs
+++ Flagship/Delegate/Delegate.cs
@@ -10,5 +10,10 @@ using System.Threading.Tasks;
 namespace Flagship.Delegate
 {
     public delegate void StatusChangeDelegate(FlagshipStatus status);
+    /// <summary>
+    /// Define an event to get callback each time a Flag has been exposed to a visitor (When a flag has been seen by your visitor) and succeeded.
+    /// </summary>
+    /// <param name="exposedVisitor">An interface to get information about the visitor to whom the flag has been exposed</param>
+    /// <param name="exposedFlag">An interface to get information about the flag that has been exposed.</param>
     public delegate void OnVisitorExposedDelegate(IExposedVisitor exposedVisitor, IExposedFlag exposedFlag);
 }

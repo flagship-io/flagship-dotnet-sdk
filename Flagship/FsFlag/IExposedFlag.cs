a787b789a8f14c7a638c3a02be4c1bdc1a3c6475
Heihokon <neoshark22@gmail.com>
2023-05-11 14:24:45 -0500

    add documents

--- Flagship/FsFlag/IExposedFlag.cs
+++ Flagship/FsFlag/IExposedFlag.cs
@@ -6,11 +6,29 @@ using System.Threading.Tasks;
 
 namespace Flagship.FsFlag
 {
+    /// <summary>
+    /// An interface to get information about the flag that has been exposed.
+    /// </summary>
     public interface IExposedFlag
     {
+        /// <summary>
+        /// Get the flag key
+        /// </summary>
         string Key { get; }
+
+        /// <summary>
+        /// Get the flag value
+        /// </summary>
         object Value { get; }
+
+        /// <summary>
+        /// Get the flag default value
+        /// </summary>
         object DefaultValue { get; }
+
+        /// <summary>
+        /// Get the flag metadata
+        /// </summary>
         IFlagMetadata Metadata { get; }
 
     }
--- Flagship/FsVisitor/IExposedVisitor.cs
+++ Flagship/FsVisitor/IExposedVisitor.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace Flagship.FsVisitor
 {
+    /// <summary>
+    /// An interface to get information about the visitor to whom the flag has been exposed
+    /// </summary>
     public interface IExposedVisitor
     {
+        /// <summary>
+        /// The key of flag
+        /// </summary>
         string Id { get; }
+
+        /// <summary>
+        /// Visitor anonymous id
+        /// </summary>
         string AnonymoudId { get; }
+
+        /// <summary>
+        /// Visitor context
+        /// </summary>
         IDictionary<string, object> Context { get; }
     }
 }

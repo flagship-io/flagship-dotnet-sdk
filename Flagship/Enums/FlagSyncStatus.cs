650dc9880beab9c6cd508aad4e8912b377a92e79
Heihokon <neoshark22@gmail.com>
2023-08-24 13:39:24 -0500

    add FlagSyncStatus enum

---
+++ Flagship/Enums/FlagSyncStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flagship.Enums
+{
+    internal enum FlagSyncStatus
+    {
+        CREATED,
+        CONTEXT_UPDATED,
+        FLAGS_FETCHED,
+        AUTHENTICATED,
+        UNAUTHENTICATED
+    }
+}

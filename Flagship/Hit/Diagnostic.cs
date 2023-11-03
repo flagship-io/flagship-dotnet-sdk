36d005075bdfc04e9909ffc34213bd84fede984d
Heihokon <neoshark22@gmail.com>
2023-11-03 16:07:34 -0500

    working [ToApiKeys method]

--- Flagship/Hit/Diagnostic.cs
+++ Flagship/Hit/Diagnostic.cs
@@ -35,6 +35,7 @@ namespace Flagship.Hit
         public bool? SdkConfigUsingCustomHitCache { get; set; }
         public bool? SdkConfigUsingCustomVisitorCache { get; set; }
         public bool? SdkConfigUsingOnVisitorExposed { get; set; }
+        public bool? SdkConfigDisableCache { get; set; }
 
         public string HttpRequestUrl { get; set; }
         public string HttpRequestMethod { get; set; }
@@ -44,18 +45,19 @@ namespace Flagship.Hit
         public string HttpResponseUrl { get; set; }
         public string HttpResponseMethod { get; set; }
         public IDictionary<string, object> HttpResponseHeaders { get; set; }
-        public int HttpResponseCode { get; set; }
+        public int? HttpResponseCode { get; set; }
         public object HttpResponseBody { get; set; }
-        public int HttpResponseTime { get; set; }
+        public int? HttpResponseTime { get; set; }
 
         public string VisitorStatus { get; set; }
-        public string VisitorInstanceType { get; set; }
+        public InstanceType? VisitorInstanceType { get; set; }
         public IDictionary<string, object> VisitorContext { get; set; }
         public bool? VisitorConsent { get; set; }
         public IDictionary<string, object> VisitorAssignmentHistory { get; set; }
         public IDictionary<string, FlagDTO> VisitorFlags { get; set; }
         public Campaign VisitorCampaigns { get; set; }
         public bool? VisitorIsAuthenticated { get; set; }
+        public string VisitorSessionId { get; set; }
 
         public string ContextKey { get; set; }
         public object ContextValue { get; set; }
@@ -174,7 +176,143 @@ namespace Flagship.Hit
 
             if (SdkConfigTrackingManagerConfigBatchIntervals != null)
             {
+                customVariable["sdk.config.trackingManager.batchIntervals"] = SdkConfigTrackingManagerConfigBatchIntervals.GetValueOrDefault().ToString();
+            }
 
+            if (SdkConfigTrackingManagerConfigPoolMaxSize != null)
+            {
+                customVariable["sdk.config.trackingManager.poolMaxSize"] = SdkConfigTrackingManagerConfigPoolMaxSize.GetValueOrDefault().ToString();
+            }
+
+            if (SdkConfigUsingCustomHitCache != null)
+            {
+                customVariable["sdk.config.usingCustomHitCache"] = SdkConfigUsingCustomHitCache.GetValueOrDefault().ToString();
+            }
+
+            if (SdkConfigUsingCustomVisitorCache != null)
+            {
+                customVariable["sdk.config.usingCustomVisitorCache"] = SdkConfigUsingCustomVisitorCache.GetValueOrDefault().ToString();
+            }
+
+            if (SdkConfigUsingOnVisitorExposed != null)
+            {
+                customVariable["sdk.config.usingOnVisitorExposed"] = SdkConfigUsingOnVisitorExposed.GetValueOrDefault().ToString();
+            }
+
+            if (SdkConfigDisableCache != null)
+            {
+                customVariable["sdk.config.disableCache"] = SdkConfigDisableCache.GetValueOrDefault().ToString();
+            }
+
+            if (HttpRequestUrl != null)
+            {
+                customVariable["http.request.url"] = HttpRequestUrl;
+            }
+
+            if (HttpRequestMethod != null)
+            {
+                customVariable["http.request.method"] = HttpRequestMethod;
+            }
+
+            if (HttpRequestHeaders != null)
+            {
+                customVariable["http.request.headers"] = JsonConvert.SerializeObject(HttpRequestHeaders);
+            }
+
+            if (HttpsRequestBody != null)
+            {
+                customVariable["http.request.body"] = JsonConvert.SerializeObject(HttpsRequestBody);
+            }
+
+            if (HttpResponseUrl != null)
+            {
+                customVariable["http.response.url"] = HttpResponseUrl;
+            }
+
+            if (HttpResponseMethod != null)
+            {
+                customVariable["http.response.method"] = HttpResponseMethod;
+            }
+
+            if (HttpResponseHeaders != null)
+            {
+                customVariable["http.response.headers"] = JsonConvert.SerializeObject(HttpResponseHeaders);
+            }
+
+            if (HttpResponseCode != null)
+            {
+                customVariable["http.response.code"] = HttpResponseCode.GetValueOrDefault().ToString();
+            }
+
+            if (HttpResponseBody != null)
+            {
+                customVariable["http.response.body"] = JsonConvert.SerializeObject(HttpResponseBody);
+            }
+
+            if (HttpResponseTime != null)
+            {
+                customVariable["http.response.time"] = HttpResponseTime.GetValueOrDefault().ToString();
+            }
+
+            if (VisitorId!=null)
+            {
+                customVariable["visitor.visitorId"] = VisitorId;
+            }
+
+            if (AnonymousId != null)
+            {
+                customVariable["visitor.anonymousId"] = AnonymousId;
+            }
+
+            if (VisitorSessionId != null)
+            {
+                customVariable["visitor.sessionId"] = VisitorSessionId;
+            }
+
+            if (VisitorStatus != null)
+            {
+                customVariable["visitor.status"] = VisitorStatus;
+            }
+
+            if (VisitorInstanceType != null)
+            {
+                customVariable["visitor.instanceType"] = $"{VisitorInstanceType}";
+            }
+
+            if (VisitorContext != null)
+            {
+                foreach (var item in VisitorContext)
+                {
+                    customVariable[$"visitor.context.{item.Key}"] = item.Value.ToString();
+                }
+            }
+
+            if (VisitorConsent != null)
+            {
+                customVariable["visitor.consent"] = VisitorConsent.GetValueOrDefault().ToString();
+            }
+
+            if (VisitorAssignmentHistory != null)
+            {
+                foreach (var item in VisitorAssignmentHistory)
+                {
+                    customVariable[$"visitor.assignments.[{item.Key}]"] = item.Value.ToString();
+                }
+            }
+
+            if (VisitorFlags != null)
+            {
+                foreach (var item in VisitorFlags)
+                {
+                    var flagDto = item.Value;
+                    var flagKey = item.Value.Key;
+                    var commonKey = $"visitor.flags.[{flagKey}]";
+                    var commonKey2 = $"{commonKey}.metadata";
+                    customVariable[$"{commonKey}.key"] = flagKey;
+                    customVariable[$"{commonKey}.value"] = JsonConvert.SerializeObject(flagDto.Value);
+                    customVariable[$"{commonKey}.metadata.campaignId"] = flagDto.CampaignId;
+
+                }
             }
 
             return apiKeys;

9680954105a7bade702201f21a2fb173e544d9d8
Heihokon <neoshark22@gmail.com>
2023-11-09 11:03:56 -0600

    add Troubleshooting class

---
+++ Flagship/Hit/Troubleshooting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Flagship.Hit
+{
+    internal class Troubleshooting : Diagnostic
+    {
+        public Troubleshooting() : base(HitType.TROUBLESHOOTING)
+        {
+
+        }
+    }
+}
+

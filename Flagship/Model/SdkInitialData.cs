1ccb4f5c643ef76bed5150f7b8ac95d6125ce3ce
Heihokon <neoshark22@gmail.com>
2023-11-09 11:06:35 -0600

    add SdkInitialData model

---
+++ Flagship/Model/SdkInitialData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Flagship.Model
+{
+	public class SdkInitialData
+	{
+		public string InstanceId { get; set; }
+		public string LastInitializationTimestamp { get; set; }
+		public bool UsingCustomHitCache { get; set; }
+		public bool UsingCustomVisitorCache { get; set; }
+	}
+}
+

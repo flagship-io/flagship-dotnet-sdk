ce41cbe894548e0261189394ed45198d20e49898
Heihokon <neoshark22@gmail.com>
2024-01-09 16:00:56 -0600

    add AccountSettings class

--- Flagship/Model/Bucketing/BucketingDTO.cs
+++ Flagship/Model/Bucketing/BucketingDTO.cs
@@ -6,9 +6,15 @@ using System.Threading.Tasks;
 
 namespace Flagship.Model.Bucketing
 {
+   internal class AccountSettings
+    {
+        public bool? EnabledXPC { get; set; }
+        public TroubleshootingData Troubleshooting { get; set; }
+    }
     internal class BucketingDTO
     {
         public bool? Panic { get; set; }
         public IEnumerable<Campaign> Campaigns { get; set; }
+        public AccountSettings  AccountSettings { get; set; }
     }
 }

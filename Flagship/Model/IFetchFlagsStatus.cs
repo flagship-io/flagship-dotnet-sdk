2e54fcd50f38012cc771c4512621e76c3e239de8
Heihokon <neoshark22@gmail.com>
2024-05-10 15:38:34 -0500

    Add IFetchFlagsStatus interface for fetching flag status reasons and status

---
+++ Flagship/Model/IFetchFlagsStatus.cs
@@ -0,0 +1,10 @@
+﻿using Flagship.Enums;
+
+namespace Flagship.Model
+{
+    public interface IFetchFlagsStatus
+    {
+        FSFetchReasons Reason { get; set; }
+        FSFetchStatus Status { get; set; }
+    }
+}

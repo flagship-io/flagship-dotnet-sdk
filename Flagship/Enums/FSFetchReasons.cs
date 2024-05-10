3b411107f4e842c5875be57ea440027c3314f97d
Heihokon <neoshark22@gmail.com>
2024-05-10 12:06:04 -0500

    feat: Add new enums for Flagship SDK status and flag status

---
+++ Flagship/Enums/FSFetchReasons.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flagship.Enums
+{
+    /// <summary>
+    /// Enum representing the reasons for fetching Flags.
+    /// </summary>
+    public enum FSFetchReasons
+    {
+        /// <summary>
+        /// Indicates that a context has been updated or changed.
+        /// </summary>
+        UPDATE_CONTEXT = 0,
+
+        /// <summary>
+        /// Indicates that the XPC method 'authenticate' has been called.
+        /// </summary>
+        AUTHENTICATE = 1,
+
+        /// <summary>
+        /// Indicates that the XPC method 'unauthenticate' has been called.
+        /// </summary>
+        UNAUTHENTICATE = 2,
+
+        /// <summary>
+        /// Indicates that fetching flags has failed.
+        /// </summary>
+        FETCH_ERROR = 3,
+
+        /// <summary>
+        /// Indicates that flags have been fetched from the cache.
+        /// </summary>
+        READ_FROM_CACHE = 4,
+
+        /// <summary>
+        /// Indicates that the visitor has been created.
+        /// </summary>
+        VISITOR_CREATED = 5,
+
+        /// <summary>
+        /// Indicates that there is no specific reason for fetching flags.
+        /// </summary>
+        NONE = 6
+    }
+}
---
+++ Flagship/Enums/FSSdkStatus.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flagship.Enums 
+{
+    /// <summary>
+    /// Enum representing the status of the Flagship SDK.
+    /// </summary>
+    public enum FSSdkStatus
+    {
+        /// <summary>
+        /// It is the default initial status. This status remains until the sdk has been initialized successfully.
+        /// </summary>
+        SDK_NOT_INITIALIZED = 0,
+
+        /// <summary>
+        /// The SDK is currently initializing.
+        /// </summary>
+        SDK_INITIALIZING = 1,
+
+        /// <summary>
+        /// Flagship SDK is ready but is running in Panic mode: All features are disabled except the one which refresh this status.
+        /// </summary>
+        SDK_PANIC = 2,
+
+        /// <summary>
+        /// The Initialization is done, and Flagship SDK is ready to use.
+        /// </summary>
+        SDK_INITIALIZED = 3
+    }
+}

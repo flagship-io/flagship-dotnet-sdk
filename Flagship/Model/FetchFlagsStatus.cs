e516fc4539394d289dd499abfd1c1ac4f658f6de
Heihokon <neoshark22@gmail.com>
2024-05-10 15:38:44 -0500

    Add FetchFlagsStatus class to represent the status of visitor fetch for flag data

---
+++ Flagship/Model/FetchFlagsStatus.cs
@@ -0,0 +1,25 @@
+﻿using Flagship.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flagship.Model
+{
+    /// <summary>
+    /// Represents the status of visitor fetch for flag data.
+    /// </summary>
+    internal class FetchFlagsStatus : IFetchFlagsStatus
+    {
+        /// <summary>
+        /// The new status of the flags fetch.
+        /// </summary>
+        public FSFetchStatus Status { get; set; }
+
+        /// <summary>
+        /// The reason for the status change.
+        /// </summary>
+        public FSFetchReasons Reason { get; set; }
+    }
+}

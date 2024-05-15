1fbd9d108a6b161fd66dc852bd239cabb926f258
Heihokon <neoshark22@gmail.com>
2024-05-15 14:23:21 -0500

    add doc tracking manager configuration classes

--- Flagship/Config/ITrackingManagerConfig.cs
+++ Flagship/Config/ITrackingManagerConfig.cs
@@ -7,18 +7,28 @@ using System.Threading.Tasks;
 
 namespace Flagship.Config
 {
+    /// <summary>
+    /// Represents the configuration of the tracking manager.
+    /// </summary>
     public interface ITrackingManagerConfig
     {
         /// <summary>
-        /// Define the time intervals the SDK will use to send tracking batches.
+        /// Gets or sets a regular interval of time to trigger batch processing
         /// </summary>
         TimeSpan BatchIntervals { get; set; }
 
         /// <summary>
-        /// Define the maximum number of tracking hit that each batch can contain.
+        /// Define the maximum number of hits the pool must reach to automatically batch all hits in the pool and send them.
+        /// <br/>
+        /// **Note**: <br/>
+        /// - Must be greater than 5 otherwise default value will be used <br/>
+        /// - Having a large PoolMaxSize can lead to performance issues
         /// </summary>
         int PoolMaxSize { get; set; }
-        CacheStrategy CacheStrategy { get; }
 
+        /// <summary>
+        /// Gets the strategy that will be used for hit caching.
+        /// </summary>
+        CacheStrategy CacheStrategy { get; }
     }
 }
--- Flagship/Config/TrackingManagerConfig.cs
+++ Flagship/Config/TrackingManagerConfig.cs
@@ -7,13 +7,25 @@ using System.Threading.Tasks;
 
 namespace Flagship.Config
 {
+    /// <summary>
+    /// Represents the configuration for the tracking manager.
+    /// </summary>
     public class TrackingManagerConfig : ITrackingManagerConfig
     {
-
+  
         public TimeSpan BatchIntervals { get; set; }
+
+
         public int PoolMaxSize { get; set; }
+
         public CacheStrategy CacheStrategy { get; set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TrackingManagerConfig"/> class with the specified cache strategy, pool max size, and batch intervals.
+        /// </summary>
+        /// <param name="cacheStrategy">The cache strategy for tracking data.</param>
+        /// <param name="poolMaxSize">The maximum size of the tracking pool.</param>
+        /// <param name="batchIntervals">The batch intervals for sending tracked data.</param>
         public TrackingManagerConfig(CacheStrategy cacheStrategy, int poolMaxSize, TimeSpan batchIntervals)
         {
             BatchIntervals = batchIntervals;
@@ -21,16 +33,28 @@ namespace Flagship.Config
             CacheStrategy = cacheStrategy;
         }
 
-        public TrackingManagerConfig():this(CacheStrategy.PERIODIC_CACHING,
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TrackingManagerConfig"/> class with default values.
+        /// </summary>
+        public TrackingManagerConfig() : this(CacheStrategy.PERIODIC_CACHING,
             Constants.DEFAULT_POOL_MAX_SIZE, TimeSpan.FromMilliseconds(Constants.DEFAULT_BATCH_TIME_INTERVAL))
         {
         }
 
-        public TrackingManagerConfig(CacheStrategy batchStrategy):this(batchStrategy, Constants.DEFAULT_POOL_MAX_SIZE, TimeSpan.FromMilliseconds(Constants.DEFAULT_BATCH_TIME_INTERVAL))
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TrackingManagerConfig"/> class with the specified batch strategy and default values for pool max size and batch intervals.
+        /// </summary>
+        /// <param name="batchStrategy">The batch strategy for sending tracked data.</param>
+        public TrackingManagerConfig(CacheStrategy batchStrategy) : this(batchStrategy, Constants.DEFAULT_POOL_MAX_SIZE, TimeSpan.FromMilliseconds(Constants.DEFAULT_BATCH_TIME_INTERVAL))
         {
            
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TrackingManagerConfig"/> class with the specified batch strategy, pool max size, and default batch intervals.
+        /// </summary>
+        /// <param name="batchStrategy">The batch strategy for sending tracked data.</param>
+        /// <param name="poolMaxSize">The maximum size of the tracking pool.</param>
         public TrackingManagerConfig(CacheStrategy batchStrategy, int poolMaxSize) : this(batchStrategy, poolMaxSize, TimeSpan.FromMilliseconds(Constants.DEFAULT_BATCH_TIME_INTERVAL))
         {
 

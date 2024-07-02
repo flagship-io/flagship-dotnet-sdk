d5999262048e75581604d2b4928976398f40bc39
Heihokon <neoshark22@gmail.com>
2024-07-02 13:37:15 -0500

    Refactor IVisitor.cs to remove unused code and improve flag handling

--- Flagship/FsVisitor/IVisitor.cs
+++ Flagship/FsVisitor/IVisitor.cs
@@ -1,14 +1,8 @@
 ﻿using Flagship.Config;
 using Flagship.Delegate;
 using Flagship.FsFlag;
-using Flagship.Hit;
 using Flagship.Model;
-using Newtonsoft.Json.Linq;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Flagship.FsVisitor
 {
@@ -17,8 +11,6 @@ namespace Flagship.FsVisitor
         string VisitorId { get; set; }
 
         string AnonymousId { get; }
-
-        ICollection<FlagDTO> Flags { get; }
 
         /// <summary>
         /// Return True if the visitor has consented for private data usage, otherwise return False.
@@ -40,39 +32,13 @@ namespace Flagship.FsVisitor
         /// <param name="key">key associated to the flag.</param>
         /// <param name="defaultValue">flag default value.</param>
         /// <returns></returns>
-        IFlag<string> GetFlag(string key, string defaultValue);
+        IFlag GetFlag(string key);
 
         /// <summary>
-        /// Return a Flag object by its key. If no flag match the given key an empty flag will be returned. Call Exists to check if the flag has been found.
+        /// Returns a collection of all flags fetched for the visitor.
         /// </summary>
-        /// <param name="key">key associated to the flag.</param>
-        /// <param name="defaultValue">flag default value.</param>
         /// <returns></returns>
-        IFlag<long> GetFlag(string key, long defaultValue);
-
-        /// <summary>
-        /// Return a Flag object by its key. If no flag match the given key an empty flag will be returned. Call Exists to check if the flag has been found.
-        /// </summary>
-        /// <param name="key">key associated to the flag.</param>
-        /// <param name="defaultValue">flag default value.</param>
-        /// <returns></returns>
-        IFlag<bool> GetFlag(string key, bool defaultValue);
-
-        /// <summary>
-        /// Return a Flag object by its key. If no flag match the given key an empty flag will be returned. Call Exists to check if the flag has been found.
-        /// </summary>
-        /// <param name="key">key associated to the flag.</param>
-        /// <param name="defaultValue">flag default value.</param>
-        /// <returns></returns>
-        IFlag<JObject> GetFlag(string key, JObject defaultValue);
-
-        /// <summary>
-        /// Return a Flag object by its key. If no flag match the given key an empty flag will be returned. Call Exists to check if the flag has been found.
-        /// </summary>
-        /// <param name="key">key associated to the flag.</param>
-        /// <param name="defaultValue">flag default value.</param>
-        /// <returns></returns>
-        IFlag<JArray> GetFlag(string key, JArray defaultValue);
+        IFlagCollection GetFlags();
 
         /// <summary>
         /// The fetch status of the flags.

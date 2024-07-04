ecc2b7d06362aa7c488be9401d8aa3d9659e2cb8
Heihokon <neoshark22@gmail.com>
2024-07-04 09:28:22 -0500

    feat: Update flag retrieval in TestCampaigns.cs
    The code changes in TestCampaigns.cs modify the way flags are retrieved using the `GetFlag` method. The `defaultValue` parameter is removed from the method calls, and the `GetValue` method is updated to include the `defaultValue` as an argument. This change improves the consistency of flag retrieval and ensures that the default value is properly used when necessary.

--- Flagship.Tests/Functional-test/TestCampaigns.cs
+++ Flagship.Tests/Functional-test/TestCampaigns.cs
@@ -30,23 +30,23 @@ namespace Flagship.Tests.Functional_test
 
             var defaultValue = "default-value";
 
-            var flag = visitor.GetFlag("ci_flag_1", defaultValue);
-            Assert.AreEqual(defaultValue, flag.GetValue());
+            var flag = visitor.GetFlag("ci_flag_1");
+            Assert.AreEqual(defaultValue, flag.GetValue(defaultValue));
             Assert.AreEqual("Test-campaign ab", flag.Metadata.CampaignName);
 
 
             visitor = Fs.NewVisitor("visitor-3", true).SetContext(context).Build();
             await visitor.FetchFlags();
 
-            flag = visitor.GetFlag("ci_flag_1", defaultValue);
-            Assert.AreEqual("flag-1-value-1", flag.GetValue());
+            flag = visitor.GetFlag("ci_flag_1");
+            Assert.AreEqual("flag-1-value-1", flag.GetValue(defaultValue));
             Assert.AreEqual("Test-campaign ab", flag.Metadata.CampaignName);
 
             visitor.UpdateContext("test-ab", false);
             await visitor.FetchFlags();
 
-            flag = visitor.GetFlag("ci_flag_1", defaultValue);
-            Assert.AreEqual(defaultValue, flag.GetValue());
+            flag = visitor.GetFlag("ci_flag_1");
+            Assert.AreEqual(defaultValue, flag.GetValue(defaultValue));
             Assert.AreEqual("", flag.Metadata.CampaignName);
         }
 
@@ -72,23 +72,23 @@ namespace Flagship.Tests.Functional_test
 
             var defaultValue = "default-value";
 
-            var flag = visitor.GetFlag("ci_flag_1", defaultValue);
-            Assert.AreEqual(defaultValue, flag.GetValue());
+            var flag = visitor.GetFlag("ci_flag_1");
+            Assert.AreEqual(defaultValue, flag.GetValue(defaultValue));
             Assert.AreEqual("Test-campaign ab", flag.Metadata.CampaignName);
 
 
             visitor = Fs.NewVisitor("visitor-3", true).SetContext(context).Build();
             await visitor.FetchFlags();
 
-            flag = visitor.GetFlag("ci_flag_1", defaultValue);
-            Assert.AreEqual("flag-1-value-1", flag.GetValue());
+            flag = visitor.GetFlag("ci_flag_1");
+            Assert.AreEqual("flag-1-value-1", flag.GetValue(defaultValue));
             Assert.AreEqual("Test-campaign ab", flag.Metadata.CampaignName);
 
             visitor.UpdateContext("test-ab", false);
             await visitor.FetchFlags();
 
-            flag = visitor.GetFlag("ci_flag_1", defaultValue);
-            Assert.AreEqual(defaultValue, flag.GetValue());
+            flag = visitor.GetFlag("ci_flag_1");
+            Assert.AreEqual(defaultValue, flag.GetValue(defaultValue));
             Assert.AreEqual("", flag.Metadata.CampaignName);
         }
 

3227a16a8d6e41e692c82c6a8d341fff15098d12
Heihokon <neoshark22@gmail.com>
2024-07-05 11:46:49 -0500

    chore: Update test-run.sh to use relative path for coverage.cobertura.xml

--- Flagship.Tests/test-run.sh
+++ Flagship.Tests/test-run.sh
@@ -4,12 +4,12 @@
 set -e
 
 # Run tests with Coverlet to collect coverage data, outputting in Cobertura format
-dotnet dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura
+dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura
 
 # Use ReportGenerator to convert the coverage report to HTML
 # Replace "coverage.cobertura.xml" with the actual path to your Cobertura coverage report if different
 # The output will be placed in a folder named "coveragereport"
-dotnet reportgenerator "-reports:coverage.cobertura.xml" "-targetdir:coveragereport" "-reporttypes:Html"
+dotnet reportgenerator "-reports:./coverage.cobertura.xml" "-targetdir:coveragereport" "-reporttypes:Html"
 
 # Open the coverage report in the default web browser (Mac specific command)
 # For Linux, you might use xdg-open, and for Windows, start

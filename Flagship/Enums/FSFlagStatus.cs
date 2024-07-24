87ea5af33ba6fa5c3937b9d12750b4d53ba6ca99
Heihokon <neoshark22@gmail.com>
2024-07-24 13:00:24 -0500

    refactor: Update FSFlagStatus.cs to improve flag status enum names

--- Flagship/Enums/FSFlagStatus.cs
+++ Flagship/Enums/FSFlagStatus.cs
@@ -17,18 +17,20 @@ namespace Flagship.Enums
         FETCHED = 0,
 
         /// <summary>
-        /// The flags need to be re-fetched due to a change in context, or because the flags were loaded from cache or XPC.
+        /// The flags are currently being fetched from the API or re-evaluated in bucketing mode.
         /// </summary>
-        FETCH_REQUIRED = 1,
+        FETCHING = 1,
 
         /// <summary>
-        /// The flag was not found or does not exist.
+        /// The flags need to be re-fetched due to a change in context, or because the flags were loaded from cache or XPC.
         /// </summary>
-        NOT_FOUND = 2,
+        FETCH_REQUIRED = 2,
 
         /// <summary>
         /// The SDK is in PANIC mode: All features are disabled except for the one to fetch flags.
         /// </summary>
         PANIC = 3,
+
+        NOT_FOUND = 4
     }
 }

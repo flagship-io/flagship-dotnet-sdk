8dfa9483a2e64a5f68b33d4af4ca2b9e5caa63e6
Heihokon <neoshark22@gmail.com>
2024-07-24 13:00:07 -0500

    refactor: Update FSFetchReasons.cs to use more descriptive enum names

--- Flagship/Enums/FSFetchReasons.cs
+++ Flagship/Enums/FSFetchReasons.cs
@@ -14,32 +14,32 @@ namespace Flagship.Enums
         /// <summary>
         /// Indicates that a context has been updated or changed.
         /// </summary>
-        UPDATE_CONTEXT = 0,
+        VISITOR_CONTEXT_UPDATED = 0,
 
         /// <summary>
         /// Indicates that the XPC method 'authenticate' has been called.
         /// </summary>
-        AUTHENTICATE = 1,
+        VISITOR_AUTHENTICATED = 1,
 
         /// <summary>
         /// Indicates that the XPC method 'unauthenticate' has been called.
         /// </summary>
-        UNAUTHENTICATE = 2,
+        VISITOR_UNAUTHENTICATED = 2,
 
         /// <summary>
         /// Indicates that fetching flags has failed.
         /// </summary>
-        FETCH_ERROR = 3,
+        FLAGS_FETCHING_ERROR = 3,
 
         /// <summary>
         /// Indicates that flags have been fetched from the cache.
         /// </summary>
-        READ_FROM_CACHE = 4,
+        FLAGS_FETCHED_FROM_CACHE = 4,
 
         /// <summary>
         /// Indicates that the visitor has been created.
         /// </summary>
-        VISITOR_CREATED = 5,
+        FLAGS_NEVER_FETCHED = 5,
 
         /// <summary>
         /// Indicates that there is no specific reason for fetching flags.

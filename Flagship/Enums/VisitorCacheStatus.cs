00537c0107c6bcc6560e85c920e089b5b9ea9ad0
Heihokon <neoshark22@gmail.com>
2024-10-15 16:17:37 -0500

    feat: Add VisitorCacheStatus enum to Flagship.Enums

---
+++ Flagship/Enums/VisitorCacheStatus.cs
@@ -0,0 +1,13 @@
+
+
+
+namespace Flagship.Enums
+{
+    internal enum VisitorCacheStatus
+    {
+        NONE,
+        ANONYMOUS_ID_CACHE,
+        VISITOR_ID_CACHE,
+        VISITOR_ID_CACHE_WITH_ANONYMOUS_ID_CACHE
+    }
+}
